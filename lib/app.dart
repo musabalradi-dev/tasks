87bcf7ec1596f04ae8090867089cb22280390769
Musab Alradi <78719015+musabalradi-dev@users.noreply.github.com>
2025-04-09 19:11:03 +0200

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tasks/core/theme/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tasks/features/layout/presentation/bloc/layout_cubit.dart';
+import 'package:tasks/features/layout/presentation/screen/layout.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeApp.lightTheme,
+      darkTheme: ThemeApp.darkTheme,
+      home: BlocProvider(
+          create: (context) => LayoutCubit()..createDataBase(),
+          child: const LayoutScreen()),
+      locale: Locale('en', 'US'),
+    );
+  }
+}
